e986a1423fd99a55c8f789a41f7eccf7ff2d5ae2
Robin <robin.code22@gmail.com>
2025-09-01 23:19:25 +0100

    fix(/homepage/src/sass/layouts/_contact.scss): fix layout of contact section on desktop and tablet

--- homepage/webpack.dev.js
+++ homepage/webpack.dev.js
@@ -1,87 +1,79 @@
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
+    entry: './src/index.mjs',
     output: {
         path: path.resolve(__dirname, 'dist'),
         publicPath: '',
-        filename: 'bundle.js'
+        filename: 'bundle.js',
     },
     devtool: "eval-source-map",
     devServer: {
-        watchFiles: ["./src/index.html", "./src/styles/*.scss", "./src/scripts/*.js"],
+        hot: true,
+        liveReload: true,
+        client: {
+            overlay: true
+        },
+        watchFiles: ["./src/*.html", "./src/sass/**/*.scss", "./src/*.{js,mjs}"],
     },
+
     module: {
         rules: [
+            {
+                test: /\.html$/i,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: {
+                            sources: {
+                                list: [
+                                    '...'
+                                ]
+                            },
+                            minimize: false
+                        }
+                    }
+                ]
+            },
             {
                 test: /\.js$/,
                 exclude: /(node_modules)/,
                 use: {
                     loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
+                    options: {presets: ['@babel/preset-env']}
                 }
             },
             {
                 test: /\.(sa|sc|c)ss$/,
-                // Set loaders to transform files.
                 use: [
-                    {
-                        // After all CSS loaders, we use a plugin to do its work.
-                        // It gets all transformed CSS and extracts it into separate
-                        // single bundled file
-                        loader: MiniCssExtractPlugin.loader
-                    },
-                    {
-                        // This loader resolves url() and @imports inside CSS
-                        loader: "css-loader",
-                    },
-                    {
-                        // Apply postCSS fixes like autoprefixer and minifying
-                        loader: "postcss-loader"
-                    },
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "postcss-loader",
                     {
                         loader: "sass-loader",
-                        options: {
-                            implementation: require("sass")
-                        }
+                        options: {implementation: require("sass")}
                     }
                 ]
             },
             {
-                test: /\.(png|jpe?g|gif|svg)$/,
-                use: [
-                    {
-                        // Using file-loader for these files
-                        loader: "file-loader",
-                        options: {
-                            outputPath: 'images'
-                        }
-                    }
-                ]
+                test: /\.(png|jpe?g|gif|svg|avif)$/i,
+                type: 'asset/resource',
             },
             {
-                test: /\.(woff|woff2|ttf|otf|eot)$/,
-                use: [
-                    {
-                        loader: "file-loader",
-                        options: {
-                            outputPath: 'fonts'
-                        }
-                    }
-                ]
+                test: /\.(woff|woff2|ttf|otf|eot)$/i,
+                type: 'asset/resource',
+                generator: {filename: 'fonts/[name][ext][query]'}
             }
         ],
     },
     plugins: [
-        new MiniCssExtractPlugin({
-            filename: "bundle.css"
-        }),
+        new MiniCssExtractPlugin({filename: "bundle.css"}),
         new HtmlWebpackPlugin({
             template: "./src/index.html",
-        }),
+            inject: 'body'
+        })
     ]
 };
